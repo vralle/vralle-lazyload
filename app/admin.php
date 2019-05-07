6f860044e7e2562f60603491ce724b56b301bccd
vralle <email4vit@gmail.com>
2019-05-08 00:18:48 +0300

    Remove escaping fields data

--- app/admin.php
+++ app/admin.php
@@ -63,10 +63,10 @@ class Admin
     public function addSettingsLink($actions)
     {
         $options_page_url  = \menu_page_url($this->plugin_name, false);
-        $settings_link = sprintf(
+        $settings_link = \sprintf(
             '<a href="%s">%s</a>',
-            \esc_url($options_page_url),
-            \esc_html__('Settings', 'vralle-lazyload')
+            $options_page_url,
+            \__('Settings', 'vralle-lazyload')
         );
         \array_unshift($actions, $settings_link);
 
@@ -103,23 +103,23 @@ class Admin
         $sections = array(
             array(
                 'id' => 'images',
-                'title' => \esc_html__('Lazy loading', 'vralle-lazyload'),
+                'title' => \__('Lazy loading', 'vralle-lazyload'),
             ),
             array(
                 'id' => 'responsive',
-                'title' => \esc_html__('Responsive images', 'vralle-lazyload'),
+                'title' => \__('Responsive images', 'vralle-lazyload'),
             ),
             array(
                 'id' => 'exclude',
-                'title' => \esc_html__('Exclude', 'vralle-lazyload'),
+                'title' => \__('Exclude', 'vralle-lazyload'),
             ),
             array(
                 'id' => 'fine_tuning',
-                'title' => \esc_html__('Fine tuning', 'vralle-lazyload'),
+                'title' => \__('Fine tuning', 'vralle-lazyload'),
             ),
             array(
                 'id' => 'extensions',
-                'title' => \esc_html__('Extensions', 'vralle-lazyload'),
+                'title' => \__('Extensions', 'vralle-lazyload'),
             ),
         );
 
@@ -182,12 +182,12 @@ class Admin
             case 'text':
                 $output .= \sprintf(
                     '<input name="%1$s[%2$s]" id="%1$s[%2$s]" class="%3$s" type="%4$s" placeholder="%5$s" value="%6$s" maxlength="120" />',
-                    \esc_attr($this->plugin_name),
-                    \esc_attr($arguments['uid']),
-                    isset($arguments['class']) ? \esc_attr($arguments['class']) : '',
-                    \esc_attr($arguments['type']),
-                    isset($arguments['placeholder']) ? \esc_attr($arguments['placeholder']) : '',
-                    esc_attr($options[$arguments['uid']])
+                    $this->plugin_name,
+                    $arguments['uid'],
+                    isset($arguments['class']) ? $arguments['class'] : '',
+                    $arguments['type'],
+                    isset($arguments['placeholder']) ? $arguments['placeholder'] : '',
+                    $options[$arguments['uid']]
                 );
                 break;
             case 'number':
@@ -197,13 +197,13 @@ class Admin
 
                 $output .= \sprintf(
                     '<input name="%1$s[%2$s]" id="%1$s[%2$s]" type="number" placeholder="%3$s"%4$s%5$s step="%6$s" value="%7$s" maxlength="12" />',
-                    \esc_attr($this->plugin_name),
-                    \esc_attr($arguments['uid']),
-                    isset($arguments['placeholder']) ? \esc_attr($arguments['placeholder']) : '',
-                    isset($arguments['min']) ? ' min="' . \esc_attr($arguments['min']) . '"' : '',
-                    isset($arguments['max']) ? ' max="' . \esc_attr($arguments['max']) . '"': '',
-                    \esc_attr($arguments['step']),
-                    esc_attr($options[$arguments['uid']])
+                    $this->plugin_name,
+                    $arguments['uid'],
+                    isset($arguments['placeholder']) ? $arguments['placeholder'] : '',
+                    isset($arguments['min']) ? ' min="' . $arguments['min'] . '"' : '',
+                    isset($arguments['max']) ? ' max="' . $arguments['max'] . '"': '',
+                    $arguments['step'],
+                    $options[$arguments['uid']]
                 );
                 break;
             case 'checkbox':
@@ -213,8 +213,8 @@ class Admin
                 }
                 $output .= \sprintf(
                     '<input type="checkbox" id="%1$s[%2$s]" name="%1$s[%2$s]" value="1" %3$s%4$s>',
-                    \esc_attr($this->plugin_name),
-                    \esc_attr($arguments['uid']),
+                    $this->plugin_name,
+                    $arguments['uid'],
                     \checked(isset($options[$arguments['uid']]), true, false),
                     $disable
                 );
@@ -230,16 +230,16 @@ class Admin
                 }
                 $output .= \sprintf(
                     '<select name="%1$s[%2$s]" id="%1$s[%2$s]"%3$s>',
-                    \esc_attr($this->plugin_name),
-                    \esc_attr($arguments['uid']),
+                    $this->plugin_name,
+                    $arguments['uid'],
                     $disable
                 );
                 foreach ($arguments['options'] as $key => $text) {
                     $output .= \sprintf(
                         '<option value="%s"%s>%s</option>',
-                        \esc_attr($key),
+                        $key,
                         \selected($key, $options[$arguments['uid']], false),
-                        \esc_attr($text)
+                        $text
                     );
                 }
                 $output .= '</select>';
@@ -251,17 +251,17 @@ class Admin
             if ('select' == $arguments['type']) {
                 $output .= \sprintf(
                     ' <label for="%1$s[%2$s]">%3$s</label>',
-                    \esc_attr($this->plugin_name),
-                    \esc_attr($arguments['uid']),
-                    \esc_attr($arguments['label'])
+                    $this->plugin_name,
+                    $arguments['uid'],
+                    $arguments['label']
                 );
             } else {
                 $output = \sprintf(
                     '<label for="%1$s[%2$s]">%3$s %4$s</label>',
-                    \esc_attr($this->plugin_name),
-                    \esc_attr($arguments['uid']),
+                    $this->plugin_name,
+                    $arguments['uid'],
                     $output,
-                    \esc_attr($arguments['label'])
+                    $arguments['label']
                 );
             }
         }
@@ -269,14 +269,14 @@ class Admin
         if (isset($arguments['description'])) {
             $output .= \sprintf(
                 '<p class="description">%s</p>',
-                \wp_kses($arguments['description'], 'default')
+                $arguments['description']
             );
         }
 
         if (isset($arguments['label']) || isset($arguments['description'])) {
             $output = \sprintf(
                 '<fieldset><legend class="screen-reader-text"><span>%s</span></legend>%s</fieldset>',
-                esc_attr($arguments['title']),
+                $arguments['title'],
                 $output
             );
         }
